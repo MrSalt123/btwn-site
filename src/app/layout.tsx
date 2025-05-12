38e31973a9da0b1905f91192a6069e093b1b868a
MrSalt123 <119909394+MrSalt123@users.noreply.github.com>
2025-05-11 22:48:42 -0400

    idk bruh

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-// import ScrollWrapper from "@/components/ScrollWrapper";
+import ScrollWrapper from "@/components/ScrollWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ScrollWrapper>
           <Navbar />
           {children}
+        </ScrollWrapper>
       </body>
     </html>
   );
