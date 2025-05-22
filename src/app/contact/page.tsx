c9a9dc0f9edbe009044c6d14516f0872b8dd7fbf
MrSalt123 <119909394+MrSalt123@users.noreply.github.com>
2025-05-22 17:27:11 -0400

    bruh

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
 export default function ContactPage() {
